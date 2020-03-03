66d8407a4898b0f27e11ca374330b29848d74808
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-03-03 15:52:38 -0700

    Use postgres

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 // const Message = require('./db/Message');
 
 // if you are using postgres, uncomment this line
-// const pool = require('./db/pgconfig');
+const pool = require('./db/pgconfig');
 
 app.use((req,res,next) => {
   res.status(404).send('That route does not exist');
