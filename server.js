36d8286ffff7efa0928e138cc7c455fb050fbe1f
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-03-03 18:21:05 -0700

    Add/debug CRUD functions

--- server.js
+++ server.js
@@ -1,20 +1,61 @@
 const express = require('express');
 const app = express();
+const bp = require('body-parser');
+// const pool = require('pg')
 
 // if you are using mongo/mongoose uncomment this line
 // const Message = require('./db/Message');
 
 // if you are using postgres, uncomment this line
+// const pool = require('./db/pgconfig');
 const pool = require('./db/pgconfig');
 
-app.use((req,res,next) => {
-  res.status(404).send('That route does not exist');
-});
+app.use(bp.json());
 
 const port = 3000;
 
+// addMessage
+app.post('/api/messages', (req, res) => {
+  pool.addMessage(req.body)
+      .then(() => res.sendStatus(201));
+});
+
+// deleteMessage
+app.delete('/api/messages/:msg_id', (req, res) => {
+  pool.deleteMessage(req.body)
+      .then(() => res.sendStatus(201));
+});
+
+// getAllMessages
+app.get('/api/messages', (req, res) => {
+  pool.getAllMessages()
+      .then(results => res.send(results));
+});
+
+// getMessage
+app.get('/api/messages/:msg_name', (req, res) => {
+  pool.getMessage(req.params.name)
+      .then(results => res.send(results));
+});
+
+// updateMessageMessage
+app.get('/api/messages/:msg_id', (req, res) => {
+  pool.putMessage(req.params.msg_message)
+      .then(results => res.send(results));
+});
+
+// updateMessageName
+app.put('/api/messages/:msg_is', (req, res) => {
+  pool.putMessage(req.params.msg_name)
+      .then(results => res.send(results));
+});
+
 app.listen(port, () => {
   console.log('Listening on port', port);
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('That route does not exist');
 });
 
 module.exports = app;
